ad1a6d3df6ea50f9966a26ada45f9b526fd8081a
Adrian <adrianfdez469@gmail.com>
2020-08-05 14:18:50 -0500

    Configurando para deploy en heroki
    Se agrego la libreria config para poder usar variables de entorno

--- mongoose.js
+++ mongoose.js
@@ -1,19 +1,16 @@
 const mongoose = require('mongoose');
+const config = require('config');
 
 
-/*const db = {
-    init: () => {
-
-    }
-};*/
-const ipDb = process.env.MONGODB_ADDON_HOST || 'localhost';
+/*const ipDb = process.env.MONGODB_ADDON_HOST || 'localhost';
 const userDb = process.env.MONGODB_ADDON_USER || '';
 const passDb = process.env.MONGODB_ADDON_PASSWORD || '';
 const nameDb = process.env.MONGODB_ADDON_DB || 'chatapp';
-const port = process.env.MONGODB_ADDON_PORT || 27017;
-
+const port = process.env.MONGODB_ADDON_PORT || 27017;*/
+const db = config.get('db');
 mongoose
-    .connect(`mongodb://${userDb ? `${userDb}:` : ''}${passDb ? `${passDb}@` : ''}${ipDb}:${port}/${nameDb}`, {useNewUrlParser: true,  useUnifiedTopology: true })
+    //.connect(`mongodb://${userDb ? `${userDb}:` : ''}${passDb ? `${passDb}@` : ''}${ipDb}:${port}/${nameDb}`, {useNewUrlParser: true,  useUnifiedTopology: true })
+    .connect(db)
     .then(connection => {
         console.log('connected to the database');
     })
