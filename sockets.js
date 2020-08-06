8bb2ab1f9b87867d73d38203fee3042ae58f907c
Adrian <adrianfdez469@gmail.com>
2020-08-06 17:43:28 -0500

    Arreglando aplicacion para que no use base de datos

--- sockets.js
+++ sockets.js
@@ -4,23 +4,32 @@ module.exports = {
     init: httpServer => {
         io = require('socket.io')(httpServer);
         io.on('connection', socket => {
+
             socket.on('new user', (params) => {
-                socket._id = params._id;
-                LoginController.bindSocketIdToUser(params._id, socket.id);
+                
+                socket.nickname = params.nickname;
                 socket.broadcast.emit('new user', {...params, socketId: socket.id});
+                
+                io.clients((error, clients) => {
+                    if(error) throw error;
+                    const dataClients = clients
+                        .map(client => {
+                            const {nickname: nick, id:socketId} = io.of('/').connected[client]
+                            return {nick, socketId};
+                        });
+                    io.to(socket.id).emit('new user', dataClients);
+                })
             });
 
             socket.on('message', ({from, toUserId, toSocketId, msg}) => {
-                // las 2 variantes funcionana
-                //socketAll[toUserId].emit('message');
-                io.to(toSocketId).emit('message',{msg, _id: socket._id});
+                io.to(toSocketId).emit('message',{msg, socketId: socket.id});
                 
             });
 
             socket.on('disconnect', async ({_id}) => {
                 
-                await LoginController.LogoutDb(socket._id);
-                socket.broadcast.emit('user disconnect', {_id: socket._id});
+                //await LoginController.LogoutDb(socket._id);
+                socket.broadcast.emit('user disconnect', {socketId: socket.id});
                 //delete socketAll[socket.id];
             });
         });
