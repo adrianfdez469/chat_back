3446a5194da2c95f71c30faaa156c6c2b2d3823b
Adrian <adrianfdez469@gmail.com>
2020-09-01 15:40:05 -0500

    Actualizando la vista

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d701e5f7d93bc41cf0cfe1041e56fafa.js"
+  "/precache-manifest.c2ecb8ef96f50cf2a276cded4a04d436.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
